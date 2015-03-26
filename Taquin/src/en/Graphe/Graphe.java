cfc0fae6c3e052a5cabeafdbb6abbd62dfaa3391
Adrien <Adrien@192.168.14.203>
2015-03-26 16:26:53 +0100

    Enlevage de code inutile

--- Taquin/src/en/Graphe/Graphe.java
+++ Taquin/src/en/Graphe/Graphe.java
@@ -61,22 +61,9 @@ public class Graphe {
 				toAdd.add(other);
 			}
 		}
-		// System.out.println(toGrow);
 		toAdd.removeAll(toDel);
 		toGrow.removeAll(toDel);
-		// System.out.println(toGrow);
 		toGrow.addAll(toAdd);
-		// System.out.println(toGrow);
-		/*
-		 * Iterator<Node> it = graphe.iterator(); ArrayList<Node> toGrow = new
-		 * ArrayList<Node>(); while (it.hasNext()) { Node node = it.next(); if
-		 * (!node.isHasSons()) { toGrow.add(node); node.setHasSons(true); } } it
-		 * = toGrow.iterator(); while (it.hasNext()) { Node node = it.next();
-		 * ArrayList<String> possibleMoves = node.possibleMoves();
-		 * Iterator<String> moves = possibleMoves.iterator(); while
-		 * (moves.hasNext()) { String dir = moves.next(); Node other = new
-		 * Node(node.makeMove(dir)); addNode(node, other); f.add(other); } }
-		 */
 	}
 
 	public Set<Node> getGraphe() {
@@ -114,9 +101,7 @@ public class Graphe {
 			n = it.next();
 		if (n != null && n.win())
 			return n;
-		// System.out.println(next);
 		grow(next);
-		// System.out.println(this.graphe);
 		return search(next);
 	}
 }
